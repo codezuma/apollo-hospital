9e7de5b457de10d97afe0e1d8b573b1180c9a96c
chandresh patidar <patidarchandresh2002@gmail.com>
2023-09-06 10:02:03 +0530

    change hraidng

--- src/components/diagnosis/feature-section.tsx
+++ src/components/diagnosis/feature-section.tsx
@@ -26,8 +26,8 @@ const FeatureSection = () => {
         <section className="bg-white container-main ">
             <div className="py-8 px-4 mx-auto  sm:py-16 lg:px-6">
                 <div className="mb-8 lg:mb-16">
-                    <h2 className="mb-4 text-4xl tracking-tight font-semibold   text-gray-900">Designed for business teams like yours</h2>
-                    <p className="text-gray-500 sm:text-xl font-semibold ">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+                    <h2 className="mb-4 text-4xl tracking-tight font-semibold   text-gray-900">Our Dedication to Excellence </h2>
+                    <p className="text-gray-500 sm:text-xl font-semibold ">Explore the core principles that define Apollo Clinic as your trusted healthcare destination. We are committed to the latest technology, a team of experienced professionals, and a convenient Gurugram location to ensure your well-being.</p>
                 </div>
                 <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
                     {apolloClinicData.map((feature, index) => (
