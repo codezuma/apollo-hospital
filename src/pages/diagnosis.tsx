db4a2654b2db344d72ef6d0f18b0ab5e46a9e079
chandresh patidar <patidarchandresh2002@gmail.com>
2023-09-28 13:08:41 +0530

    feat: change gtag

--- src/pages/diagnosis.tsx
+++ src/pages/diagnosis.tsx
@@ -45,15 +45,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11108610629" />
-          <Script id="google-analytics" strategy="afterInteractive" >{`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-CDEK34SL2X"/>
 
-  gtag('config', 'AW-11108610629');
+<Script id="google-analytics" strategy="afterInteractive" >{`
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-CDEK34SL2X');
 `}
-      </Script>
+</Script>
       <HeroSection></HeroSection>
       <FeatureSection />
       <AboutSection></AboutSection>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,14 +48,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11108610629" />
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-CDEK34SL2X"/>
 
   <Script id="google-analytics" strategy="afterInteractive" >{`
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'AW-11108610629');
+  gtag('config', 'G-CDEK34SL2X');
   `}
   </Script>
       <HeroSection></HeroSection>
