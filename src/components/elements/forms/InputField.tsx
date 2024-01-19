a9b4d86af7ab8028d05ff3e5b46e44bc4ddedef6
Chandresh patidar <patidarchandresh2002@gmail.com>
2024-01-19 16:19:32 +0530

    faet: ad

--- src/components/elements/forms/InputField.tsx
+++ src/components/elements/forms/InputField.tsx
@@ -4,12 +4,13 @@
     interface InputFieldProps extends React.HTMLAttributes<HTMLInputElement>{
         Icon:any,
         name:string,
+        placeholder?:string,
         type:string,
         required:boolean
     } 
     export default function InputField(props:InputFieldProps) {
 
-        const {name,required,Icon,type,onChange} = props;   
+        const {name,required,Icon,type,onChange,placeholder} = props;   
         return <>
             <div className="w-full px-4 ">
                 <div className="md:mb-8 mb-4">
@@ -17,7 +18,7 @@
                         {name.split("-").join(" ")}<span className="text-red-500">{required? '*':''}</span>
                     </label>
                     <div className="relative">
-                        <input required={required} type={type} onChange={onChange} name={name}  className={`w-full rounded-xl border border-form-stroke p-3 pl-12 text-primary placeholder-[#929DA7] outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-[#F5F7FD]`} />
+                        <input placeholder={placeholder} required={required} type={type} onChange={onChange} name={name}  className={`w-full rounded-xl border border-form-stroke p-3 pl-12 text-primary placeholder-[#929DA7] outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-[#F5F7FD]`} />
                         <span className="absolute top-1/2 left-4 -translate-y-1/2">
                             <Icon className="w-6 h-6 text-gray-300"></Icon>
                         </span>
